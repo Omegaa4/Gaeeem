9c85b5f01500abc61d2b15a9b22d9b9ce232beb9
AlexTheBowlerHat <85088627+AlexTheBowlerHat@users.noreply.github.com>
2021-07-09 12:06:58 +1200

    script additions (tilemap test)
    multiple scripts were added to game objects, mostly from brackeys tutorials, and a scripts folder was created

---
+++ tilemap test/Assets/Scripts/ItemPickup.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Script was written by Alexander Yong, modified and adapted from the 10 August 2017 Brackeys Item Script Tutorial
+public class ItemPickup : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
